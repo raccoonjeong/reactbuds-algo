0420a4067d10c370d4233f99599e63a76ab88e07
raccoonjeong <rana3782@gmail.com>
2026-02-12 15:37:34 +0900

    백준 1037 약수

---
+++ day7/Baekjoon1037.java
@@ -0,0 +1,24 @@
+
+
+// 심화
+// 약수 
+// https://www.acmicpc.net/problem/1037
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        sc.nextLine();
+        int[] divisors = new int[count];
+        for (int i = 0; i < count; i++) {
+            divisors[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(divisors);
+
+        System.out.println(divisors[0] * divisors[count - 1]);
+    }
+}
